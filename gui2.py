6c61235a61e045ed39682ae7431a5857bfb5223a
MichaelxhJiang <michaelxhj@outlook.com>
2016-09-17 23:36:54 -0400

    merged files

--- gui2.py
+++ gui2.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 import ttk
+import analyze
 
 root = Tk()
 
@@ -45,3 +46,18 @@ content.columnconfigure(4, weight=1)
 content.rowconfigure(1, weight=1)
 
 root.mainloop()
+
+def decode (lst):
+    #text = text from textfield
+    newText = ""
+    #bold keywords
+    for key in lst:
+        for word in key["position"]:
+            newText += text[:word["position"] + 1]
+            text = text[:word["position"]]  #truncate original text
+            newText += "<b>" + text[:key["word"]] + "</b>" #bold keyword
+            text = text[:text[:key["word"] + 1] #truncate original text
+        newText += text #add remaining text
+        text = newText #reset text for next wave of bolding
+                
+    return newText
